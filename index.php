e4affbb7227e94ed1d930081685fcbddb121e578
bofod <info@computrix.dk>
2012-10-23 07:51:31 +0300

    Update index.php
    Added a semicolon and changed get_options to get_option.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  $options = get_options('plugin_options')
-  header( "HTTP/1.1 301 Moved Permanently" ); 
-  header( "Location: " . $options['url'] ); 
-?> 
+  $options = get_option('plugin_options');
+  header( "HTTP/1.1 301 Moved Permanently" );
+  header( "Location: " . $options['url'] );
+?>
